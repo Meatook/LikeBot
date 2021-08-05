5dd2ed73215074fef4b77555f8723760a092c7ad
1majic <58144991+1majic@users.noreply.github.com>
2021-08-05 20:07:58 +0500

    Update liker.py

--- liker.py
+++ liker.py
@@ -2,7 +2,7 @@ import vk_api
 from time import sleep
 from random import randint
 
-login, password = 
+login, password = '', ''
 vk_session = vk_api.VkApi(login, password)
 vk_session.auth()
 vk = vk_session.get_api()
@@ -12,17 +12,17 @@ def get_liked(post_id):
     if not check_like(post_id):
         sleep(4)
         vk_session.method('likes.add',
-                          {'user_id': 353313939, 'type': 'post', 'owner_id': -193376219, 'item_id': post_id})
+                          {'user_id': , 'type': 'post', 'owner_id': , 'item_id': post_id})
 
 
 def check_like(post_id):
     a = vk_session.method('likes.isLiked',
-                          {'user_id': 353313939, 'type': 'post', 'owner_id': -193376219, 'item_id': post_id})
+                          {'user_id': , 'type': 'post', 'owner_id': , 'item_id': post_id})
     return a['liked']
 
 
 def get_last_post_id():
-    all_wall = (vk_session.method('wall.get', {'owner_id': -193376219, 'count': 2})).values()
+    all_wall = (vk_session.method('wall.get', {'owner_id': , 'count': 2})).values()
     last_post_id = (list(list(all_wall)[1])[1])['id']
     return last_post_id
 
